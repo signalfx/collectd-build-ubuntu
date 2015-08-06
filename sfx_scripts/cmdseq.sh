417e1955fd3dfc7937b1d103f1419fe84fb03320
udaysagar2177 <udaysagar.2177@gmail.com>
2015-08-05 19:42:48 -0700

    Added sfx tag to the version number

--- sfx_scripts/cmdseq.sh
+++ sfx_scripts/cmdseq.sh
@@ -15,6 +15,17 @@ patch -p0 < debian/patches/precise_control.patch
 patch -p0 < debian/patches/precise_rules.patch
 fi
 
+#patch version-gen.sh
+VERSION_TAG="$(head -1 debian/changelog | awk -F"[-~]" 'NF>2{print $2}')"
+
+_NEW_VERSION=$(grep DEFAULT_VERSION= version-gen.sh  | awk -F\" '{ print $2; }' | perl -p -e "s/git/$VERSION_TAG/g;")
+cat <<HERE > version-gen.sh
+#!/bin/sh
+printf "$_NEW_VERSION"
+HERE
+
+chmod 755 version-gen.sh
+
 ./build.sh
 DIST=${DISTRIBUTION} ARCH=amd64 yes | debuild -us -uc -S
 rm -rf /opt/mount/pbuilder_results/
