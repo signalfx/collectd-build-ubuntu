660d724610c92cb3ca60e97d25646063ab827486
Matthew Pound <mwp@signalfx.com>
2015-11-04 14:56:53 -0800

    build dogstatsd

--- build-plugin/debian/deps.sh
+++ build-plugin/debian/deps.sh
@@ -1,5 +1,9 @@
-rm -f plugins/requirements.txt
+set -ex
 tar -xvzf psutil-3.2.2.tar.gz
-cd psutil-3.2.2
-python setup.py build
-mv build/lib*/psutil ../plugins/.
+(cd psutil-3.2.2 && python setup.py build && mv build/lib*/psutil ../plugins/. )
+
+tar -xvzf simplejson-3.8.1.tar.gz
+(cd simplejson-3.8.1 && python setup.py build && mv build/lib*/simplejson ../plugins/. )
+
+find venv
+(cd venv/lib/python*/*packages && mv pyformance requests google signalfx ../../../../plugins/. )
